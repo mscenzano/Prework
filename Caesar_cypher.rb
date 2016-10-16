96f08c3d91f81ca0d37d92a82a58005e0df9a38c
Mariano Saenz de Cenzano <mscenzano@gmail.com>
2016-10-16 18:25:03 +0200

    Add files via upload

---
+++ Caesar_cypher.rb
@@ -0,0 +1,36 @@
+
+
+puts "type the phrase you want to cypher:"
+
+answer = gets.chomp
+caesar_cypher = []
+ascii_result = 0
+
+answ_arr = answer.split
+
+answ_arr.each do |word|
+	word.each_char do |character|
+		char_ascii = character.ord
+		if char_ascii > 96 && char_ascii < 123 
+			char_ascii += 3
+			character_result =char_ascii.chr
+
+		elsif char_ascii > 64 && char_ascii < 91
+			char_ascii += 3
+			character_result = char_ascii.chr
+
+		end
+
+		caesar_cypher << letraresult
+
+	end	
+
+	caesar_cypher << " "
+end	
+
+	puts "Thanks to Caesar Cypher, the final result is ---> #{caesar_cypher.join}" 
+	puts "Alea Jact Est"
+
+
+
+
