5ca499a5292b291cea00655aae8d1b36a907220c
Mariano Saenz de Cenzano <mscenzano@gmail.com>
2016-10-17 11:49:04 +0200

    First Javascript exercices

---
+++ first_js_scripts.js
@@ -0,0 +1,63 @@
+/Fist JS Scripts/
+
+console.log("This is a message");
+
+console.log("This needs to be in quotes or it won't work!");
+console.log("Help! I'm trapped in the computer.");
+
+console.log("Mariano" + 6); 
+
+
+
+/var + changing the var value + concatenation of strings/
+
+var city
+
+city = "Madrid";
+
+console.log(city);
+
+city = "Barcelona" 
+
+console.log(city);
+
+city = city + " ciudad condal"; 
+
+console.log(city); 
+
+
+
+/Test of Fixnum + String/
+
+var test
+
+test = 16
+
+test = test + "Hi, what´s up?"
+
+console.log(test);
+
+
+/Test of Symbols/ 
+
+var testing
+
+testing =  "$"; 
+
+testing = testing + " I need a lot of those" ; 
+
+console.log(testing); 
+
+
+/la la la --> "for" loop / 
+
+
+var i;
+
+
+for (i = "la";  i <= "lalalalalalalalalala";  i = i + "la") {
+    console.log(i);
+}
+
+
+
